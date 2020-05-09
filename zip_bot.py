fd3ac1789e154f9c0f57b5f22d84987bc1de52e0
SHUO-PC\ShuoZ <landzs@hotmail.com>
2020-05-09 20:22:04 +0200

    Add zip_bot.py and files to zip bot for ai arena

---
+++ zip_bot.py
@@ -0,0 +1,24 @@
+# create a ZipFile object
+from zipfile import ZipFile
+import os
+
+
+def zip_folder(dirname):
+    # Iterate over all the files in directory
+    for folderName, subfolders, filenames in os.walk(dirname):
+        for filename in filenames:
+            #create complete filepath of file in directory
+            filePath = os.path.join(folderName, filename)
+            # Add file to zip
+            zip_file.write(filePath)
+
+
+with ZipFile('Fire.zip', 'w') as zip_file:
+    zip_file.write("__init__.py")
+    zip_file.write("ladderbots.json")
+    zip_file.write("run.py")
+    
+    zip_folder(r"./sc2")
+    zip_folder(r"./bot")
+    zip_folder(r"./basic_manager")
+    
