35ad51e6b10425435e9e2081c7fd8d8e2d643f58
SHUO-PC\ShuoZ <landzs@hotmail.com>
2020-05-11 19:40:27 +0200

    Add resources manager

---
+++ basic_manager/resources_manager.py
@@ -0,0 +1,317 @@
+import inspect
+from sc2.unit import Unit
+from sc2.ids.unit_typeid import UnitTypeId
+from sc2.ids.ability_id import AbilityId
+
+
+class TerranResourcesManager:
+    """
+    - Basic resources Management for Terran
+    """
+
+    def __init__(self, bot=None, priority_manager=None):
+        self.bot = bot
+        self.priority_manager = priority_manager
+
+    async def manage_resources(self, iteration):
+        """
+        - Manage resource, call in on_step
+        - Including distribute workers, build resource unit and structure
+
+        parm: iteration
+        """
+        if iteration % 10 == 0:
+            await self.distribute_workers(self.bot.resource_ratio)
+        await self.build_workers()
+        await self.build_refineries()
+        await self.build_base()
+        await self.build_orbital()
+        await self.manage_orbital()
+
+    async def build_workers(self):
+        if not self.bot.priority_manager.check_block(
+            inspect.currentframe().f_code.co_name
+        ):
+            if (
+                self.bot.can_afford(UnitTypeId.SCV)
+                and self.bot.supply_left > 0
+                and self.bot.supply_workers <= self.bot.workers_limit
+            ):
+                for th in self.bot.townhalls.ready.idle:
+                    if self.bot.can_afford(UnitTypeId.SCV):
+                        th.train(UnitTypeId.SCV)
+
+    async def build_base(self):
+        if not self.bot.priority_manager.check_block(
+            inspect.currentframe().f_code.co_name
+        ):
+            if (
+                self.bot.townhalls_limit > self.bot.townhalls.amount
+                and self.bot.already_pending(UnitTypeId.COMMANDCENTER) == 0
+                and self.bot.can_afford(UnitTypeId.COMMANDCENTER)
+            ):
+                await self.bot.expand_now()
+
+    async def build_refineries(self):
+        if not self.bot.priority_manager.check_block(
+            inspect.currentframe().f_code.co_name
+        ):
+            for th in self.bot.townhalls.ready:
+                vgs = self.bot.vespene_geyser.closer_than(10, th)
+                for vg in vgs:
+                    if (
+                        (await self.bot.can_place(
+                            UnitTypeId.REFINERY, [vg.position]))[0]
+                        and self.bot.can_afford(UnitTypeId.REFINERY)
+                    ):
+                        worker = self.bot.select_build_worker(vg)
+                        if worker:
+                            worker.build(UnitTypeId.REFINERY, vg)
+                            self.bot.priority_manager.block("build_refineries")
+                            break
+
+    async def build_orbital(self):
+        if not self.bot.priority_manager.check_block(
+            inspect.currentframe().f_code.co_name
+        ):
+            if self.bot.tech_requirement_progress(
+                    UnitTypeId.ORBITALCOMMAND) == 1:
+                for cc in self.bot.townhalls(UnitTypeId.COMMANDCENTER).idle:
+                    if self.bot.can_afford(UnitTypeId.ORBITALCOMMAND):
+                        cc(AbilityId.UPGRADETOORBITAL_ORBITALCOMMAND)
+
+    async def manage_orbital(self):
+        for oc in self.bot.townhalls(UnitTypeId.ORBITALCOMMAND).filter(
+            lambda x: x.energy >= 50
+        ):
+            mfs = self.bot.mineral_field.closer_than(10, oc)
+            if mfs:
+                mf: Unit = max(mfs, key=lambda x: x.mineral_contents)
+                oc(AbilityId.CALLDOWNMULE_CALLDOWNMULE, mf)
+
+    def harvesting_efficiency(self, resource_ratio: float = 3):
+        """
+        - Return mineral efficiency,gas efficiency and number of workers
+        needed to be adjusted
+        - Number of workers needed to be adjusted is the worker's number
+        needed to be switch to gas or mineral to reach the resource_ratio
+        - When workers_to_adjust is positive, it means we need send workers
+        to gas. When negative, send workers to mineral
+
+        param: resource_ratio
+        """
+
+        mineral_efficiency: float = 0
+        gas_efficiency    : float = 0
+        mineral_per_worker: float = 42
+        gas_per_worker    : float = 40
+        mineral_per_mule  : float = 170
+
+        townhalls         = self.bot.townhalls.ready
+        gas_buildings     = self.bot.gas_buildings.ready
+        workers_to_adjust = 0
+
+        mineral_efficiency = sum(
+            min(mineral_place.assigned_harvesters,
+                mineral_place.ideal_harvesters)
+            * mineral_per_worker
+            for mineral_place in townhalls
+        )
+
+        gas_efficiency = 0.1 + sum(
+            min(gas_place.assigned_harvesters,
+                gas_place.ideal_harvesters)
+            * gas_per_worker
+            for gas_place in gas_buildings
+        )
+
+        mule_efficiency = self.bot.units(
+            UnitTypeId.MULE).amount * mineral_per_mule
+
+        mineral_efficiency += mule_efficiency
+
+        workers_to_adjust = round(
+            (mineral_efficiency - resource_ratio * gas_efficiency)
+            / (mineral_per_worker + resource_ratio * gas_per_worker)
+        )
+
+        return [mineral_efficiency, gas_efficiency, workers_to_adjust]
+
+    async def distribute_workers(self, resource_ratio: float = 3):
+        """
+        - Distributes workers across all the bases taken.
+        - Keyword `resource_ratio` takes a float. If the current
+        harvesting efficiency of minerals to gas ratio is bigger
+        than `resource_ratio`, this function prefer filling
+        gas_buildings first, if it is lower, it will prefer
+        sending workers to minerals first.
+
+        param: resource_ratio
+        """
+
+        if (
+            not self.bot.mineral_field
+            or not self.bot.workers
+            or not self.bot.townhalls.ready
+        ):
+            return
+
+        bases = self.bot.townhalls.ready
+        gas_buildings = self.bot.gas_buildings.ready
+        mining_places = bases | gas_buildings
+        worker_pool = [
+            worker
+            for worker in self.bot.workers.idle
+            if self.bot.workers_proxyrax.count(worker) == 0
+        ]
+        deficit_mining_places = []
+        current_efficiency = self.harvesting_efficiency(resource_ratio)
+        current_resource_ratio = (
+            current_efficiency[0]
+            / current_efficiency[1]
+        )
+
+        # distribute worker from surplus mining places to deficit place
+        surplus_mining_places = [
+            sp
+            for sp in mining_places
+            if sp.surplus_harvesters != 0
+        ]
+        for sp in surplus_mining_places:
+            difference = sp.surplus_harvesters
+            if sp.has_vespene:
+                local_workers = self.bot.workers.filter(
+                    lambda unit: unit.order_target == sp.tag
+                    or (
+                        unit.is_carrying_vespene
+                        and unit.order_target == bases.closest_to(sp).tag
+                    )
+                )
+            else:
+                local_minerals_tags = {
+                    mineral.tag
+                    for mineral in self.bot.mineral_field
+                    if mineral.distance_to(sp) <= 8
+                }
+                local_workers = self.bot.workers.filter(
+                    lambda unit: unit.order_target in local_minerals_tags
+                    or (
+                        unit.is_carrying_minerals
+                        and unit.order_target == sp.tag
+                    )
+                )
+            if difference > 0:
+                worker_pool += local_workers[:difference]
+            else:
+                deficit_mining_places += [sp for _ in range(-difference)]
+
+        if len(worker_pool) > len(deficit_mining_places):
+            all_minerals_near_base = [
+                mineral
+                for mineral in self.bot.mineral_field
+                if any(
+                    mineral.distance_to(base) <= 8
+                    for base in self.bot.townhalls.ready
+                )
+            ]
+
+        for worker in worker_pool:
+            if deficit_mining_places:
+                if current_resource_ratio < resource_ratio:
+                    possible_mining_places = [
+                        place
+                        for place in deficit_mining_places
+                        if not place.vespene_contents
+                    ]
+                else:
+                    possible_mining_places = [
+                        place
+                        for place in deficit_mining_places
+                        if place.vespene_contents
+                    ]
+                if not possible_mining_places:
+                    possible_mining_places = deficit_mining_places
+                current_place = min(
+                    deficit_mining_places, key=lambda place: place.distance_to(
+                        worker)
+                )
+                deficit_mining_places.remove(current_place)
+                if current_place.vespene_contents:
+                    worker.gather(current_place)
+                else:
+                    local_minerals = (
+                        mineral
+                        for mineral in self.bot.mineral_field
+                        if mineral.distance_to(current_place) <= 8
+                    )
+                    target_mineral = max(
+                        local_minerals,
+                        key=lambda mineral: mineral.mineral_contents,
+                        default=None,
+                    )
+                    if target_mineral:
+                        worker.gather(target_mineral)
+            elif worker.is_idle and all_minerals_near_base:
+                target_mineral = min(
+                    all_minerals_near_base,
+                    key=lambda mineral: mineral.distance_to(worker),
+                )
+                worker.gather(target_mineral)
+            else:
+                pass
+
+        # distribute worker based on resource ratio
+        workers_to_adjust = self.harvesting_efficiency(resource_ratio)[2]
+        potential_places = gas_buildings if workers_to_adjust >= 0 else bases
+        surplus_mining_places = [
+            sp
+            for sp in potential_places
+            if sp.surplus_harvesters != 0
+        ]
+
+        for sp in surplus_mining_places:
+            if workers_to_adjust != 0:
+                difference = sp.surplus_harvesters
+                if not sp.has_vespene:
+                    local_workers = self.bot.workers.filter(
+                        lambda unit: unit.order_target == sp.tag
+                        or (
+                            unit.is_carrying_vespene
+                            and unit.order_target == bases.closest_to(sp).tag
+                        )
+                    )
+                    worker_to_swtitch = min(
+                        -difference, -workers_to_adjust, local_workers.amount
+                    )
+                    [
+                        worker.gather(sp)
+                        for worker in local_workers[:worker_to_swtitch]
+                    ]
+                    workers_to_adjust -= difference
+                else:
+                    local_minerals_tags = {
+                        mineral.tag
+                        for mineral in self.bot.mineral_field
+                        if mineral.distance_to(sp) <= 8
+                    }
+                    local_workers = self.bot.workers.filter(
+                        lambda unit: unit.order_target in local_minerals_tags
+                        or (
+                            unit.is_carrying_minerals
+                            and unit.order_target == sp.tag
+                        )
+                    )
+                    worker_to_swtitch = min(
+                        -difference, workers_to_adjust, local_workers.amount
+                    )
+                    [
+                        worker.gather(sp)
+                        for worker in local_workers[:worker_to_swtitch]
+                    ]
+                    workers_to_adjust -= worker_to_swtitch
+
+        already_adjusted = 3 * self.bot.already_pending(UnitTypeId.REFINERY)
+        if (workers_to_adjust - already_adjusted) > 0:
+            self.bot.priority_manager.allow("build_refineries")
+        else:
+            self.bot.priority_manager.block("build_refineries")
--- zip_bot.py
+++ zip_bot.py
@@ -5,9 +5,9 @@ import os
 
 def zip_folder(dirname):
     # Iterate over all the files in directory
-    for folderName, subfolders, filenames in os.walk(dirname):
+    for folderName, filenames in os.walk(dirname):
         for filename in filenames:
-            #create complete filepath of file in directory
+            # create complete filepath of file in directory
             filePath = os.path.join(folderName, filename)
             # Add file to zip
             zip_file.write(filePath)
@@ -17,8 +17,7 @@ with ZipFile('Fire.zip', 'w') as zip_file:
     zip_file.write("__init__.py")
     zip_file.write("ladderbots.json")
     zip_file.write("run.py")
-    
+
     zip_folder(r"./sc2")
     zip_folder(r"./bot")
     zip_folder(r"./basic_manager")
-    
